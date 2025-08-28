fc4c30a9e9dc546f910b375d9c65466447282e15
Choong-geun <cgyoon91@gmail.com>
2025-08-28 21:52:59 +0900

    Add files via upload

---
+++ designkoreai2024/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+  $('#atw').click(function() {
+    alert('프로그램을 준비중입니다.');
+  });
+  $('#isp').click(function() {
+    alert('프로그램을 준비중입니다.');
+  });
+  $('#wgl').click(function() {
+    alert('프로그램을 준비중입니다.');
+  });
+  $('#nah').click(function() {
+    alert('프로그램을 준비중입니다.');
+  });
+  $('#hat').click(function() {
+    alert('프로그램을 준비중입니다.');
+  });
+});
+
+// $(document).ready(function() {
+//   $('.amu1').click(function() {
+//   	$('.amu2').toggle();
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $('.amu2').click(function() {
+//   	$('.amu3').fadeToggle();
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $('.amu3').click(function() {
+//   	$('.amu4').slideToggle();
+//   });
+// });
+
+// $(document).ready(function() {
+//   $('.amu4').click(function() {
+//   	$('.amu1').toggleClass('magnify');
+//   	$('.amu2').toggleClass('magnify');
+//   	$('.amu3').toggleClass('magnify');
+//   	$('.amu4').toggleClass('magnify');
+//   	$('.amu5').toggleClass('magnify');
+//   	$('.amu6').toggleClass('magnify');
+//   });
+// });
+
+// $(document).ready(function() {
+//   $('.amu5').click(function() {
+//   	$(this).text('   무아   ');
+//   });
+// });
+
+// $(document).ready(function() {
+//   $('.amu6').click(function() {
+//   	alert('1. 어떤 사람을 특별히 정하지 않고 이르는 인칭 대명사. 흔히 부정의 뜻을 가진 서술어와 호응하나, ‘나’, ‘라도’와 같은 조사와 함께 쓰일 때는 긍정의 뜻을 가진 서술어와 호응하기도 한다.');
+//   	alert('2. (성(姓) 다음에 쓰여) 어떤 사람을 구체적인 이름 대신 이르는 인칭 대명사.');
+//   	alert('3. 어떤 사람이나 사물 따위를 특별히 정하지 않고 이를 때 쓰는 말.');
+//   	alert('4. (주로 뒤에 오는 ‘않다’, ‘없다’, ‘못 하다’ 따위의 부정적인 말과 함께 쓰여) =아무런(‘전혀 어떠한’의 뜻을 나타내는 말).');
+//   });
+// });
+
+
+
+// $(document).ready(function() {
+//   $('.ah').click(function() {
+//     alert('1. 놀라거나, 당황하거나, 초조하거나, 다급할 때 가볍게 내는 소리.');
+//     alert('2. 기쁘거나, 슬프거나, 뉘우치거나, 칭찬할 때 가볍게 내는 소리');
+//     alert('3. 말을 하기에 앞서 상대편의 주의를 끌기 위하여 가볍게 내는 소리');
+//     alert('4. (억양을 내렸다 올리면서) 모르던 것을 깨달을 때 내는 소리.');
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $('.mu').click(function() {
+//     alert('1. 십자화과의 한해살이풀 또는 두해살이풀. 줄기는 높이가 60~100cm이며, 잎은 깃 모양으로 뿌리에서 뭉쳐나고 뿌리는 둥글고 길다. 뿌리는 잎과 함께 식용하며 비타민, 단백질의 함유량이 많아 약용하기도 한다. 중앙아시아가 원산지로 아시아, 유럽 등지의 온대에서 많은 품종이 재배된다. ≒나복2(蘿蔔)ㆍ내복8(萊菔)ㆍ노복4(蘆菔)ㆍ청근. (Raphanus sativus)(菁根)');
+//     alert('2. 없거나 존재하지 않는 상태.');
+//     alert('3. 천간(天干)의 다섯째.');
+//   });
+// });
+
+
