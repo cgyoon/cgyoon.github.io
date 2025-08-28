d4bfb76373f574f2aae321c80a556879f3ea7ffa
Choong-geun <cgyoon91@gmail.com>
2025-08-28 22:20:42 +0900

    Add files via upload

---
+++ yeontralpark/script.js
@@ -0,0 +1,83 @@
+$('#scene').on("click", function(){
+  if( $('.scene').is(':hidden')) {
+    $('.scene').show();} 
+  else {
+    $('.scene').hide();}
+});
+
+$('#night').on("click", function(){
+  if( $('.night').is(':hidden')) {
+    $('.night').show();} 
+  else {
+    $('.night').hide();}
+});
+
+$('#grass').on("click", function(){
+  if( $('.grass').is(':hidden')) {
+    $('.grass').show();} 
+  else {
+    $('.grass').hide();}
+});
+
+$('#tree').on("click", function(){
+  if( $('.tree').is(':hidden')) {
+    $('.tree').show();} 
+  else {
+    $('.tree').hide();}
+});
+
+$('#flower').on("click", function(){
+  if( $('.flower').is(':hidden')) {
+    $('.flower').show();} 
+  else {
+    $('.flower').hide();}
+});
+
+$('#rock').on("click", function(){
+  if( $('.rock').is(':hidden')) {
+    $('.rock').show();} 
+  else {
+    $('.rock').hide();}
+});
+
+$('#water').on("click", function(){
+  if( $('.water').is(':hidden')) {
+    $('.water').show();} 
+  else {
+    $('.water').hide();}
+});
+
+$('#bird').on("click", function(){
+  if( $('.bird').is(':hidden')) {
+    $('.bird').show();} 
+  else {
+    $('.bird').hide();}
+});
+
+$('#dog').on("click", function(){
+  if( $('.dog').is(':hidden')) {
+    $('.dog').show();} 
+  else {
+    $('.dog').hide();}
+});
+
+$('#bench').on("click", function(){
+  if( $('.bench').is(':hidden')) {
+    $('.bench').show();} 
+  else {
+    $('.bench').hide();}
+});
+
+$('#object').on("click", function(){
+  if( $('.object').is(':hidden')) {
+    $('.object').show();} 
+  else {
+    $('.object').hide();}
+});
+
+$('#sign').on("click", function(){
+  if( $('.sign').is(':hidden')) {
+    $('.sign').show();} 
+  else {
+    $('.sign').hide();}
+});
